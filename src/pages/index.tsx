b905eb11c2ec329920a6a80119859d636f1360b3
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-08-24 18:01:10 -0400

    built search bar and button using Shadcn, render successfully

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import SearchBar from "../../components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <SearchBar />;
 }
