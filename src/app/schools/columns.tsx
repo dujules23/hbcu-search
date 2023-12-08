00a41ca2bb61c661842f8115203fb375df381cc5
Durrell Jules <dujules23@gmail.com>
2023-12-07 19:43:10 -0500

    updated columns, rendered dummy data successfully in table

--- src/app/schools/columns.tsx
+++ src/app/schools/columns.tsx
@@ -4,51 +4,24 @@ import { ColumnDef } from "@tanstack/react-table";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
+export type School = {
   id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
+  institution: string;
+  specialization: string;
+  location: string;
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<School>[] = [
   {
-    accessorKey: "status",
-    header: () => <div className="">Status</div>,
-    cell: ({ row }) => {
-      const status = parseFloat(row.getValue("status"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(status);
-
-      return <div className=" font-medium">{formatted}</div>;
-    },
+    accessorKey: "institution",
+    header: () => <div className="">Institution</div>,
   },
   {
-    accessorKey: "email",
-    header: () => <div className="">Email</div>,
-    cell: ({ row }) => {
-      const email = parseFloat(row.getValue("email"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(email);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
+    accessorKey: "specialization",
+    header: () => <div className="">Specialization</div>,
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
+    accessorKey: "location",
+    header: () => <div className="">Location</div>,
   },
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,26 @@
 import { Inter } from "next/font/google";
 import SearchBar from "../../components/SearchBar";
 import { DataTable } from "../app/schools/DataTable";
-import { Payment, columns } from "../app/schools/columns";
+import { School, columns } from "../app/schools/columns";
 import Navbar from "../../components/common/nav/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // const data = getData();
-  // data.then((res) => {
-  //   console.log(res);
-  // });
-
-  // console.log(getData());
+  const data = [
+    {
+      id: 1,
+      institution: "Benedict College",
+      specialization: "Technology",
+      location: "Columbia, SC",
+    },
+  ];
 
   return (
     <>
       <Navbar />
       <SearchBar />
-      <DataTable columns={columns} data={[]} />
+      <DataTable columns={columns} data={data} />
     </>
   );
 }
