8aeff08481ae8e9548ec2a83c3d71708051eeae3
Durrell Jules <dujules23@gmail.com>
2023-12-13 23:24:44 -0500

    added form, need to change positioning

--- components/common/nav/Navbar.tsx
+++ components/common/nav/Navbar.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { APP_NAME } from "../AppHead";
 import { HiLightBulb } from "react-icons/hi";
+import { AddSchoolForm } from "../../../src/app/schools/AddSchoolForm";
 
 interface Props {}
 
 const Navbar: FC<Props> = (props): JSX.Element => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const openForm = () => {
+    if (!isClicked) {
+      setIsClicked(false);
+    }
+    setIsClicked(true);
+  };
+
   return (
     <div className="flex items-center justify-between p-6">
       {/* Title */}
@@ -17,8 +27,10 @@ const Navbar: FC<Props> = (props): JSX.Element => {
       <div className="flex items-center space-x-4">
         {/* Add a School Button*/}
         <div className="border p-3 rounded hover:text-black hover:bg-white transition ease-in-out">
-          <button>Add a School</button>
+          <button onClick={openForm}>Add a School</button>
         </div>
+
+        {isClicked && <AddSchoolForm setIsClicked={setIsClicked} />}
 
         {/* Dark Mode Button */}
         <div className="flex items-center space-x-5 hover:text-yellow-400 transition ease-in-out">
