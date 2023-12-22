bc001a296f40bc7cfe4bb5d75f28fa5816dfe6b1
Durrell Jules <dujules23@gmail.com>
2023-12-21 23:58:24 -0500

    added search dialog, on search bar click pops up search with results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import { SearchDialog } from "./SearchDialog";
+import { SearchCheck } from "lucide-react";
 
 interface Props {}
 
 const SearchBar: FC<Props> = (props): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  const openSearchDialog = () => {
+    console.log("this works");
+  };
+
   return (
     <div className="flex items-center justify-center p-7 mt-1">
       <div className="pr-2 ">
         <Input
+          onClick={() => setOpen(true)}
           className="w-72 rounded border-gray-700 hover:bg-gray-700 transition ease-in-out cursor-pointer"
           placeholder="Search..."
         />
       </div>
+      <SearchDialog open={open} setOpen={setOpen} />
       {/* <div>
         <Button
           className="w-32 rounded hover:bg-white hover:text-black transition ease-in-out"
---
+++ src/components/SearchDialog.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import * as React from "react";
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+interface Props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function SearchDialog({ open, setOpen }: Props) {
+  return (
+    <>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search for a school..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem className="hover:bg-gray-500 cursor-pointer">
+              <Calendar className="mr-2 h-4 w-4" />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile className="mr-2 h-4 w-4" />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <Calculator className="mr-2 h-4 w-4" />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator className="" />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
