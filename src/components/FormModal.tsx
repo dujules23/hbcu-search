9be21f2b91a3793c05a1ca35bda5d0f5a7b40557
Durrell Jules <dujules23@gmail.com>
2023-12-26 23:56:49 -0500

    adjusted modal, still working on final version

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,16 +1,31 @@
 import { FC } from "react";
 import ModalContainer, { ModalProps } from "./ModalContainer";
 import { AddSchoolForm } from "@/app/schools/AddSchoolForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogHeader } from "./ui/dialog";
 
 interface Props extends ModalProps {
-  // visible?: boolean;
+  isOpen: boolean;
   onCancel?(): void;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const FormModal: FC<Props> = ({ visible, onClose, onCancel }): JSX.Element => {
+const FormModal: FC<Props> = ({
+  visible,
+  onClose,
+  onCancel,
+  isOpen,
+  setIsOpen,
+}): JSX.Element => {
   return (
     <>
-      <ModalContainer visible={visible} onClose={onClose}>
+      {/* <ModalContainer visible={visible} onClose={onClose}>
         <div className="flex flex-col">
           <button
             className="p-2 ml-72 hover:bg-red-400 rounded-xl transition ease-in"
@@ -20,7 +35,20 @@ const FormModal: FC<Props> = ({ visible, onClose, onCancel }): JSX.Element => {
           </button>
           <AddSchoolForm />
         </div>
-      </ModalContainer>
+      </ModalContainer> */}
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger>Add a School</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            {/* <DialogTitle>Are you sure absolutely sure?</DialogTitle> */}
+            {/* <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription> */}
+            <AddSchoolForm />
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
--- src/components/common/nav/Navbar.tsx
+++ src/components/common/nav/Navbar.tsx
@@ -6,6 +6,13 @@ import { AddSchoolForm } from "../../../app/schools/AddSchoolForm";
 import FormModal from "@/components/FormModal";
 import { Form } from "react-hook-form";
 import { ModalProps } from "@/components/ModalContainer";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogHeader } from "@/components/ui/dialog";
 
 interface Props {}
 
@@ -22,35 +29,37 @@ const Navbar: FC<Props> = (): JSX.Element => {
   };
 
   return (
-    <div className="flex items-center justify-between p-6">
-      {/* Title */}
-      <Link href="/">
-        <div className="flex items-center space-x-2">
-          <span>{APP_NAME}</span>
-        </div>
-      </Link>
-      <div className="flex items-center space-x-4">
-        {/* Add a School Button*/}
-        <div className="border p-3 rounded hover:text-black hover:bg-white transition ease-in-out">
-          <button onClick={() => setIsOpen(true)}>Add a School</button>
-        </div>
+    <>
+      <div className="flex items-center justify-between p-6">
+        {/* Title */}
+        <Link href="/">
+          <div className="flex items-center space-x-2">
+            <span>{APP_NAME}</span>
+          </div>
+        </Link>
+        <div className="flex items-center space-x-4">
+          {/* Add a School Button*/}
+          <div className="border p-3 rounded hover:text-black hover:bg-white transition ease-in-out">
+            <button onClick={() => setIsOpen(true)}>Add a School</button>
+          </div>
+          <></>
 
-        {isOpen && (
           <FormModal
-            visible={isOpen}
-            onClose={() => setIsOpen(false)}
-            onCancel={() => setIsOpen(false)}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            // onClose={() => setIsOpen(false)}
+            // onCancel={() => setIsOpen(false)}
           />
-        )}
 
-        {/* Dark Mode Button */}
-        <div className="flex items-center space-x-5 hover:text-yellow-400 transition ease-in-out">
-          <button>
-            <HiLightBulb size={34} />
-          </button>
+          {/* Dark Mode Button */}
+          <div className="flex items-center space-x-5 hover:text-yellow-400 transition ease-in-out">
+            <button>
+              <HiLightBulb size={34} />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
