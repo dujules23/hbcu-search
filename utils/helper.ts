e41aec57cc0e9827b78c1d0b0506fc2f1a27eb71
Durrell Jules <dujules23@gmail.com>
2024-01-06 22:16:59 -0500

    removed old code, working on rework for grabing slugs

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,20 +1,3 @@
-// need helper function to help get form data
-// export const generateFormData = (values: z.infer<typeof formSchema>) => {
-//   const formData = new FormData();
-//   for (let key in post) {
-//     const value = (post as any)[key];
-//     if (key === "tags" && value.trim()) {
-//       // loops through array to remove blank spaces
-//       const tags = value.split(",").map((tag: string) => tag.trim());
-//       formData.append("tags", JSON.stringify(tags));
-//     } else {
-//       formData.append(key, value);
-//     }
-//   }
-
-//   return formData;
-// };
-
 // function that trims SchoolText
 export const trimSchoolText = (text: string) => {
   const newString = text
