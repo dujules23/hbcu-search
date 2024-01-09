50465b04c67a6a8f98adf3e2931c7862a35fba93
Durrell Jules <dujules23@gmail.com>
2024-01-08 21:56:46 -0500

    began working on school card

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -9,6 +9,8 @@ import {
 import School from "../../models/schoolSchema";
 import dbConnect from "../../lib/dbConnect";
 import { trimSchoolText } from "../../utils/helper";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 // interface Props {
 //   name?: string;
@@ -20,8 +22,31 @@ type Props = InferGetStaticPropsType<typeof getStaticProps>;
 const SchoolPage: NextPage<Props> = ({ school }) => {
   const { id, name, link, location, slug } = school;
 
-  console.log(school);
-  return <DefaultLayout>School Page</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <div className="">
+        <div className="flex justify-center p-8 space-x-6">
+          {/* School Card */}
+          <div className="bg-white text-gray-400 text-xl rounded p-8 w-1/3 h-52">
+            <div>{name}</div>
+            <a
+              className="hover:underline"
+              target="_blank"
+              rel="noreferrer"
+              href={`//${link}`}
+            >
+              Visit School
+            </a>
+          </div>
+          {/* Info Card  */}
+          <div>{location}</div>
+        </div>
+        <div className="flex justify-center border mb-4 w-12">
+          <Button size="default">Back</Button>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default SchoolPage;
