41487e50d401512b0c45a7352d19fa4d5f84bf89
Durrell Jules <dujules23@gmail.com>
2024-09-27 21:41:04 -0400

    removed test case

--- src/pages/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Home from "../pages/index";
-
-test("The title should be displayed", async () => {
-  render(<Home />);
-  expect(screen.getByText("HBCU Database")).toBeInTheDocument();
-});
